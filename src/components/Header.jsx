fef06bcfc443ae52c357c6f90e9a5da1d4f0b209
hairflicks <bweidner1998@gmail.com>
2023-04-20 15:47:59 +0100

    small css change to work beter on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import dice from "../imgs/dice.png"
 
 export default function Header() {
     return (
     <header>
         <h1>Roll & Review</h1>
+        <img id="dice" src={dice} alt="a single die"></img>
     </header>
     )
 }
