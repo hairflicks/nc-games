75fc70817b3fbc1020b37dc4586ac4dd68a0a8ca
hairflicks <bweidner1998@gmail.com>
2023-04-21 14:02:18 +0100

    changed header to a tag to link to homepage from every page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import dice from "../imgs/dice.png"
 export default function Header() {
     return (
     <header>
-        <h1>Roll & Review</h1>
+        <a href="/">Roll & Review</a>
         <img id="dice" src={dice} alt="a single die"></img>
     </header>
     )
